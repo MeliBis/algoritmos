777165d363f4a3e6e1bebefeed969600341ef0b9
Melissa Barrios <melissa.blecona@gmail.com>
2023-02-04 13:40:47 -0600

    numero entero invertido

---
+++ invertido.js
@@ -0,0 +1,24 @@
+//dado un número entero, inviertelo y devuelve de nuevo el entero
+
+/* ejemplo:
+invertirNumero(67) --> devuelve 76
+
+como hacerlo:
+- funcion que reciba un numero
+- convertir numero en string
+-crear un array por cada letra
+-darle la vuelta
+-unir el array en un string
+-convertir el string en un entero
+*/
+
+function invertir(numero){
+    let invertido = parseInt(numero
+                    .toString()
+                    .split('')
+                    .reverse()
+                    .join('')
+)
+   return invertido;
+}
+console.log(invertir(85));
